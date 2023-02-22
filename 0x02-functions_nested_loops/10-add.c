ff907a55884ec6dcd35258dd1e6dac93db28755c
Eugene-Eyram-Kobe <eugeneeyram.wealthy@gmail.com>
2023-02-22 07:03:48 -0800

    Nested loops project

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - returns the sum of it's parameters
+ * @a: int type number
+ * @b: int type number
+ * Returns:0
+ */
+
+
+int add(int a, int b)
+{
+	return (a+b);
+}
